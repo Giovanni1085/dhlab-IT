98a8444a15f7d1a4d6d1ab7e96d20f505c82d504
Benoit Seguin <seg.benoit@gmail.com>
2015-11-13 16:12:30 +0100

    Installation scripts of the IC clusters and one notebook example of python and Spark

---
+++ scripts/script-DHLAB.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+
+# Install LDAP + Autmount
+wget -O script-ldap.sh https://www.dropbox.com/s/a2dxg0xdooe42ma/script-ldap.sh?dl=0
+chmod a+x script-ldap.sh
+./script-ldap.sh
+rm script-ldap.sh
+echo "+ : root (DHLAB-unit) (IC-IT-unit): ALL" >> /etc/security/access.conf
+echo "- : ALL : ALL" >> /etc/security/access.conf
+
+# Format and partition scratch space
+curl -s http://install.iccluster.epfl.ch/scripts/it/scratchVolume.sh  >> scratchVolume.sh ; chmod +x scratchVolume.sh ; ./scratchVolume.sh
+
+# Mount NAS shares
+apt-get install -y cifs-utils
+# ATTENTION!!!!! No folder with a white space in it!! like "Le Temps" or "Linked Books"
+for folder in {"cluster-nas","homes","Garzoni","le_temps_data"}; do  #No space in the list! 
+mkdir "/mnt/$folder"
+cat <<EOF >> /etc/fstab
+//dhlabsrv1.epfl.ch/$folder /mnt/$folder cifs _netdev,iocharset=utf8,username=cluster,password=QXA{1f,sec=ntlmssp,gid=100 0 0
+EOF
+done
+mount -a
+
+# Install MATLAB 8.5a
+curl -s http://install.iccluster.epfl.ch/scripts/soft/matlab/matlab85a.sh  >> matlab85a.sh ; chmod +x matlab85a.sh; ./matlab85a.sh
+
+#generic dependencies
+apt-get install -y g++ git nfs-common htop screen vim unzip
+
+# python ftw
+apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
+# scikit-learn, could be built from source as well
+apt-get install -y python-sklearn
+
+# caffe dependencies (http://caffe.berkeleyvision.org/install_apt.html)
+#apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler 
+#apt-get install -y --no-install-recommends libboost-all-dev
+#apt-get install -y libatlas-base-dev
+#apt-get install -y libopenblas-dev
+# for ubuntu 14.04
+#apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
---
+++ scripts/script-ldap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+apt-get update
+apt-get install debconf-utils -y -qq
+cat <<EOF | debconf-set-selections
+nslcd   nslcd/ldap-bindpw       password
+nslcd   nslcd/ldap-sasl-mech    select
+nslcd   nslcd/ldap-sasl-authcid string
+nslcd   nslcd/ldap-sasl-authzid string
+nslcd   nslcd/ldap-auth-type    select  none
+libnss-ldapd    libnss-ldapd/clean_nsswitch     boolean false
+libnss-ldapd:amd64      libnss-ldapd/clean_nsswitch     boolean false
+nslcd   nslcd/ldap-reqcert      select
+nslcd   nslcd/ldap-base string  c=ch
+libnss-ldapd    libnss-ldapd/nsswitch   multiselect     group, passwd, shadow
+libnss-ldapd:amd64      libnss-ldapd/nsswitch   multiselect     group, passwd, shadow
+nslcd   nslcd/ldap-sasl-realm   string
+nslcd   nslcd/ldap-sasl-krb5-ccname     string  /var/run/nslcd/nslcd.tkt
+nslcd   nslcd/ldap-uris string  ldap://ldap.epfl.ch
+nslcd   nslcd/ldap-binddn       string
+nslcd   nslcd/ldap-starttls     boolean false
+nslcd   nslcd/ldap-sasl-secprops        string
+EOF
+apt-get -y install libpam-ldapd libnss-ldapd nss-updatedb libnss-db nscd nslcd ldap-utils -qq
+apt-get -y install python-ldap -qq
+apt-get -y install cifs-utils -qq
+apt-get -y install sysv-rc-conf -qq
+apt-get -y install python-pam -qq
+apt-get -y install python-lockfile -qq
+cd /tmp
+wget http://siipc20.epfl.ch/download/scripts-auto.tar.gz
+cd /
+tar xzf /tmp/scripts-auto.tar.gz
+cd
+/etc/init.d/nslcd restart
+/etc/init.d/nscd restart
