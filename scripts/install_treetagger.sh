f3bd73eb6520dfbdcdff48252a6ca004d76879fd
Matteo Romanello <matteo.romanello@gmail.com>
2016-03-18 17:22:52 +0100

    Update install_treetagger.sh

--- scripts/install_treetagger.sh
+++ scripts/install_treetagger.sh
@@ -16,9 +16,9 @@ wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/portuguese-par
 wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-par-linux-3.2-utf8.bin.gz
 # parameter files for chunker
 wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-chunker-par-linux-3.2-utf8.bin.gz
-http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-chunker-par-linux-3.2-utf8.bin.gz
-http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-chunker-par-linux-3.2-utf8.bin.gz
-http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-chunker-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/french-chunker-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/german-chunker-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/spanish-chunker-par-linux-3.2-utf8.bin.gz
 # let's run the installation script
 chmod +x install-tagger.sh 
 ./install-tagger.sh
